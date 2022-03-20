429df302641681a41048c47fd83d53d7a34c29d5
optimizion <optimizion@gmail.com>
2022-03-20 17:50:14 +0900

    A+B Problem

---
+++ cpp/A+B Problem.cpp
@@ -0,0 +1,20 @@
+// Write a program to find the sum of the two integers A and B.
+
+/* input
+    1
+    2
+*/
+/* output
+    3
+*/
+
+#include <cstdio>
+
+int main(void) {
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d\n", a + b);
+  
+    return 0;
+}
